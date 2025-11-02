6a81e06dd0611510413de2c4ea998d5e2cbab478
yello <yellowbear360@gmail.com>
2025-11-03 00:01:43 +0100

    Display prompt and read user input

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main(void)
+{
+    char command[1024];
+    while (1)
+    {
+        printf("¥ ");
+        fflush(stdout);
+        if (fgets(command, sizeof(command), stdin) == NULL)
+        {
+            printf("\n");
+            break;
+        }
+
+        command[strcspn(command, "\n")] = '\0';
+        if (strcmp(command, "exit") == 0)
+        {
+            break;
+        }
+
+        printf("You entered: %s\n", command);
+    }
+
     return 0;
 }
