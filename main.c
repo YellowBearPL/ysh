ffd50d4cdfd27cfc2b302139ac5bd75569da4eff
yello <yellowbear360@gmail.com>
2025-11-03 00:58:55 +0100

    Print appropriate error message

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main(void)
 {
     char command[1024];
+    char *args[64];
     while (1)
     {
         printf("¥ ");
@@ -20,7 +25,34 @@ int main(void)
             break;
         }
 
-        printf("You entered: %s\n", command);
+        int i = 0;
+        args[i] = strtok(command, " ");
+        while (args[i] != NULL && i < 63)
+        {
+            args[++i] = strtok(NULL, " ");
+        }
+
+        args[i] = NULL;
+        if (args[0] == NULL)
+        {
+            continue;
+        }
+
+        pid_t pid = fork();
+        if (pid == 0)
+        {
+            execvp(args[0], args);
+            perror("command not found");
+            exit(1);
+        }
+        else if (pid > 0)
+        {
+            wait(NULL);
+        }
+        else
+        {
+            perror("fork failed");
+        }
     }
 
     return 0;
