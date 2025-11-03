4ed58b6c2ae68e0fcece7d4d22dd74722c33f5e6
yello <yellowbear360@gmail.com>
2025-11-03 01:28:02 +0100

    Print absolute path

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int isBuiltin(const char *cmd)
 {
@@ -14,6 +16,38 @@ int isBuiltin(const char *cmd)
     }
 
     return 0;
+}
+
+char *findInPath(const char *cmd)
+{
+    char *pathEnv = getenv("PATH");
+    if (!pathEnv)
+    {
+        return NULL;
+    }
+
+    char *pathCopy = strdup(pathEnv);
+    if (!pathCopy)
+    {
+        return NULL;
+    }
+
+    char *dir = strtok(pathCopy, ":");
+    static char fullpath[1024];
+    while (dir != NULL)
+    {
+        snprintf(fullpath, sizeof(fullpath), "%s/%s", dir, cmd);
+        if (access(fullpath, X_OK) == 0)
+        {
+            free(pathCopy);
+            return fullpath;
+        }
+
+        dir = strtok(NULL, ":");
+    }
+
+    free(pathCopy);
+    return NULL;
 }
 
 int main(void)
@@ -78,19 +112,41 @@ int main(void)
                 continue;
             }
 
-            if (isBuiltin(args[1]))
+            const char *target = args[1];
+            if (isBuiltin(target))
             {
-                printf("%s is a shell builtin\n", args[1]);
+                printf("%s is a shell builtin\n", target);
+                continue;
+            }
+
+            char *path = findInPath(target);
+            if (path)
+            {
+                printf("%s is %s\n", target, path);
             }
             else
             {
-                printf("%s: not found\n", args[1]);
+                printf("%s: not found\n", target);
             }
 
             continue;
         }
 
-        printf("%s: not found\n", args[0]);
+        pid_t pid = fork();
+        if (pid == 0)
+        {
+            execvp(args[0], args);
+            perror("command not found");
+            exit(1);
+        }
+        else if (pid > 0)
+        {
+            wait(NULL);
+        }
+        else
+        {
+            perror("fork failed");
+        }
     }
 
     return 0;
